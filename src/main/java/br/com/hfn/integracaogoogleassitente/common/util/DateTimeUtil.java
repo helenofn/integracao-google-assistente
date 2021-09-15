0b344403eb21c0c7d3e4974a661067f2674b3af4
Heleno Freitas Neto <heleno.freitas@gmail.com>
2021-09-15 20:58:09 -0300

    atualizando fuso

--- src/main/java/br/com/hfn/integracaogoogleassitente/common/util/DateTimeUtil.java
+++ src/main/java/br/com/hfn/integracaogoogleassitente/common/util/DateTimeUtil.java
@@ -1,6 +1,7 @@
 package br.com.hfn.integracaogoogleassitente.common.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
@@ -10,8 +11,9 @@ public class DateTimeUtil {
 	public static final String DH_FRMT_DDMMYYY_HHMM = "dd/MM/yyyy HH:mm";
 	
 	public static String getDataHoraCorrenteAsString(String formato) {
-		LocalDateTime currentDateTime = LocalDateTime.now();
+		LocalDateTime currentDateTime = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formato);
 		return currentDateTime.format(formatter);
 	}
+	
 }
